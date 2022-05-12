54be0d58d47bbeb977a157054736234c47976d13
Luan Santos <instrutorluan1@gmail.com>
2022-05-12 07:46:25 -0300

    Project Success

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+/* Scroll reveal - 
+Mostrar o elemento quando der scroll na página */
+
+const scrollReveal = ScrollReveal({
+  origin: "top",
+  distance: "24px",
+  duration: 700,
+  reset: true,
+});
+
+scrollReveal.reveal(
+  `
+.s-hero .title, 
+.s-success .tag ,
+.title-success,
+.cards,
+.card,
+.s-about,
+.about-me .my-picture,
+.s-cases,
+.title-cases,
+.cases,
+.case,
+.s-contact,
+.title-contact,
+.tag .form-contact`,
+  { interval: 100 }
+);
